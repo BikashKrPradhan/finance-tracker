bde73bc85c2e9b113dffbe8a8787e9d7d01c5b70
Bikash <143929282+BikashKrPradhan@users.noreply.github.com>
2026-02-11 00:22:08 +0530

    fix mobile view

--- src/lib/getCurrentUser.ts
+++ src/lib/getCurrentUser.ts
@@ -1,13 +1,23 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./prisma";
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+    ],
+    session: {
+      strategy: "database",
+    },
+  });
 
-  if (!session?.user?.email) {
-    return null;
-  }
+  if (!session?.user?.email) return null;
 
   return prisma.user.findUnique({
     where: { email: session.user.email },
